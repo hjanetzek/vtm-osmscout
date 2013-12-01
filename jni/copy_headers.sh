d915ed6f6e90f425b743406df4cdb37ca4d0c4d4
Hannes Janetzek <hannes.janetzek@gmail.com>
2013-12-01 03:40:30 +0100

    fix copy location

--- jni/copy_headers.sh
+++ jni/copy_headers.sh
@@ -1,3 +1,3 @@
 cp include/config/Config.h  libosmscout/libosmscout/include/osmscout/private/
-cp include/config/CoreFeatures.h  libosmscout/libosmscout/include/osmscout/private/
+cp include/config/CoreFeatures.h  libosmscout/libosmscout/include/osmscout/
 cp include/config/MapFeatures.h  libosmscout/libosmscout-map/include/osmscout/
